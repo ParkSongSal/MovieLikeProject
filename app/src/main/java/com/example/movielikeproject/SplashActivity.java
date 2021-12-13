e75b9d2c9bc894dd2450b3569a31cb4c4a8ab582
SangMook Park <tkdanr0311@gmail.com>
2021-12-13 14:41:09 +0900

    1) color 연보라색 파스텔톤으로 수정 2) 로딩화면 추가

---
+++ app/src/main/java/com/example/movielikeproject/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.movielikeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startLoading();
+    }
+
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);   // 2초후에 메인화면으로 이동
+    }
+}
